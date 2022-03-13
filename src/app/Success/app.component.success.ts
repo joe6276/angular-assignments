06aff83855fac944e3080b9a6905c4190fea11c6
Jonathan Ndambuki <joendambuki16@gmail.com>
2022-03-13 12:36:44 +0300

    Worked on assignment one

---
+++ src/app/Success/app.component.success.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl:'./app.component.success.html',
+    styleUrls:['./app.component.success.css'],
+    selector:'app-success'
+})
+
+
+
+export class SuccessComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { SuccessComponent } from './Success/app.component.success';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
